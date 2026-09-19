208725aa9743e43c0d4825c451a7150eb1038d5c
Swapnil-Kashyap <kashyapswapnil007@gmail.com>
2026-09-19 15:41:28 +0530

    Time: 1 ms (87.16%), Space: 43.2 MB (99.91%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,15 +1,21 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        result.add(new ArrayList<>());
-        for(int num:nums){
-            int size = result.size();
-            for(int i=0; i<size;i++){
-                List<Integer> newSubset = new ArrayList<>(result.get(i));
-                newSubset.add(num);
-                result.add(newSubset);
-            }
+    static void solve(int[] nums,int index,List<Integer> output,List<List<Integer>>ans){
+        if(index >= nums.length){
+            ans.add(new ArrayList<>(output));
+            return;
         }
-        return result;
+        int currValue = nums[index];
+        output.add(currValue);
+        solve(nums,index+1,output,ans);
+        output.remove(output.size()-1);
+        solve(nums, index+1,output, ans);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+       List<List<Integer>>ans = new ArrayList<>();
+       List<Integer> output = new ArrayList<>();
+       int index = 0;
+       solve(nums, 0, output, ans);
+
+       return ans ;
     }
 }
